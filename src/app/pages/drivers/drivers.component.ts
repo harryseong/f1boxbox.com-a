5952cc03379a08a1d8c9d9195389ae928f946577
Harry Seong <harryseong@gmail.com>
2021-07-23 14:49:01 -0500

    Cleanup contentAnimation code.

--- src/app/pages/drivers/drivers.component.ts
+++ src/app/pages/drivers/drivers.component.ts
@@ -11,7 +11,7 @@ import {BehaviorSubject, Observable} from 'rxjs';
     trigger('contentAnimations', [
       transition(':enter', [
         style({ opacity: 0, transform: 'translateY(0.5em)'}),
-        animate('0.2s 0.2s ease', style({ opacity: 1, transform: 'translateY(0)' })),
+        animate('0s 0.2s ease', style({ opacity: 1, transform: 'translateY(0)' })),
       ]),
     ])
   ]
--- src/app/pages/teams/teams.component.ts
+++ src/app/pages/teams/teams.component.ts
@@ -11,7 +11,7 @@ import {ErgastApiService} from '../../core/services/api/ergast/ergast-api.servic
     trigger('contentAnimations', [
       transition(':enter', [
         style({ opacity: 0, transform: 'translateY(0.5em)'}),
-        animate('0.2s 0.2s ease', style({ opacity: 1, transform: 'translateY(0)' })),
+        animate('0s 0.2s ease', style({ opacity: 1, transform: 'translateY(0)' })),
       ]),
     ])
   ]
